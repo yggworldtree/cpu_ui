283645588fd970231f1fd667971441d8b9ad7e64
Linsk.Oldbook <mgr9525@qq.com>
2021-06-21 18:44:43 +0800

    lens+

--- server/route.go
+++ server/route.go
@@ -61,6 +61,26 @@ func procs(c *gin.Context) {
 	c.Data(200, "application/json", req.ResBodyBytes())
 }
 
+func warnln(c *gin.Context) {
+	req, err := YwtEgn.HbtpGrpcRequest(bean.NewCliGroupPath("mgr", "cpu-report"), 1, "GetWarnLen")
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+	defer req.Close()
+	err = req.Do(Mgr.Ctx, nil)
+	if err != nil {
+		c.String(500, "req.Do err:%v", err)
+		return
+	}
+	if req.ResCode() != hbtp.ResStatusOk {
+		c.String(500, "res code(%d) err:%s", req.ResCode(), string(req.ResBodyBytes()))
+		return
+	}
+	c.JSON(200, hbtp.Map{
+		"count": string(req.ResBodyBytes()),
+	})
+}
 func warns(c *gin.Context) {
 	req, err := YwtEgn.HbtpGrpcRequest(bean.NewCliGroupPath("mgr", "cpu-report"), 1, "GetWarns")
 	if err != nil {
@@ -78,6 +98,26 @@ func warns(c *gin.Context) {
 		return
 	}
 	c.Data(200, "application/json", req.ResBodyBytes())
+}
+func winfoln(c *gin.Context) {
+	req, err := YwtEgn.HbtpGrpcRequest(bean.NewCliGroupPath("mgr", "cpu-report"), 1, "GetInfoLen")
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+	defer req.Close()
+	err = req.Do(Mgr.Ctx, nil)
+	if err != nil {
+		c.String(500, "req.Do err:%v", err)
+		return
+	}
+	if req.ResCode() != hbtp.ResStatusOk {
+		c.String(500, "res code(%d) err:%s", req.ResCode(), string(req.ResBodyBytes()))
+		return
+	}
+	c.JSON(200, hbtp.Map{
+		"count": string(req.ResBodyBytes()),
+	})
 }
 func winfos(c *gin.Context) {
 	req, err := YwtEgn.HbtpGrpcRequest(bean.NewCliGroupPath("mgr", "cpu-report"), 1, "GetInfos")
